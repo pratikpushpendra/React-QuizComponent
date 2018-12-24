9dfe2451f2a70f99cc6445bb02db9a6424271d07
Pratik Pushpendra <pushpendra.pratik@gmail.com>
2018-12-24 15:21:56 +0530

    Completed Module 5

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton'
+import { throws } from 'assert';
 
 class QuizQuestion extends Component{
+    handleClick(button_text){
+      if(button_text === this.props.quiz_question.answer){
+        this.props.showNextQuestionHandler();
+      }
+    }
     render(){
         return(
             <main>
-            <section>
+            <section className="QuizQuestion">
               <p>{this.props.quiz_question.instruction_text}</p>
             </section>
             <section className="buttons">
               <ul>
-                <QuizQuestionButton button_text = {this.props.quiz_question.answer_options[0]}/>
+                {this.props.quiz_question.answer_options.map((answer_option, index) => {
+                  return <QuizQuestionButton key = {index} button_text = {answer_option}
+                  clickHandler = {this.handleClick.bind(this)}
+                  />
+                })}
               </ul>
             </section>
           </main>
